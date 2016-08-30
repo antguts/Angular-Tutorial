893464760b0a6d3dcf9a34e08585db0c2feb5a64
an g <an g>
2016-08-30 21:57:44 +0100

    Finish events tutorial.

--- js/exam4.js
+++ js/exam4.js
@@ -1,6 +1,20 @@
-var app2 = angular.module('app2', []);
+var app4 = angular.module('app4', []);
 
-app2.controller('ctrl1', function($scope) {
+app4.controller('eventCtrl', function($scope) {
 
+  $scope.blur = 0;
+  $scope.click = 0;
+  $scope.dblclick = 0;
+  $scope.copy = 0;
+  $scope.paste = 0;
+  $scope.cut = 0;
+  $scope.focus = 0;
+  $scope.change = 0;
 
+  $scope.keydown = function(e){
+    $scope.kdKey= String.fromCharCode(e.keyCode);
+  }
+
+  $scope.mouseenter=0;
+  $scope.mouseleave=0;
 });
