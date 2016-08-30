c8c461c125d7f2403e8170690591d0a6ae548f61
an g <an g>
2016-08-31 00:26:58 +0100

    Finish event conditional handling, move to dynamic class.

--- js/exam4.js
+++ js/exam4.js
@@ -2,19 +2,16 @@ var app4 = angular.module('app4', []);
 
 app4.controller('eventCtrl', function($scope) {
 
-  $scope.blur = 0;
-  $scope.click = 0;
-  $scope.dblclick = 0;
-  $scope.copy = 0;
-  $scope.paste = 0;
-  $scope.cut = 0;
-  $scope.focus = 0;
-  $scope.change = 0;
+  $scope.dayTimeButton = true;
 
-  $scope.keydown = function(e){
-    $scope.kdKey= String.fromCharCode(e.keyCode);
-  }
-
-  $scope.mouseenter=0;
-  $scope.mouseleave=0;
+  $scope.capitals = [
+    {"City": "Montgomery",
+    "State": "Alabama"},
+    {"City": "Jenuau",
+    "State": "Alaska"},
+    {"City": "Phoenix",
+    "State": "Arizona"},
+    {"City": "Little Rock",
+    "State": "Arkansas"}
+  ];
 });
